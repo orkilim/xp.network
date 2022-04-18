40f2c326cf29fa313ce0344e932d6825e2ced93c
Or <orkilim@gmail.com>
2022-04-18 16:33:21 +0300

    first and last commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+
 import './App.css';
 
+import { AgGridReact } from 'ag-grid-react';
+
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+
 function App() {
+  const [rowData] = useState([
+    { ID:"123456789",Name:"Inigo Montoia",Extra_Hours:5, Manual_Hours:5,Monthly_Hours:150,Overall_Hours:155},
+    { ID:"021456789",Name:"Patrick Star",Extra_Hours:0, Manual_Hours:0,Monthly_Hours:70,Overall_Hours:70},
+    { ID:"523416709",Name:"Bill Gates",Extra_Hours:10, Manual_Hours:10,Monthly_Hours:200,Overall_Hours:210}
+    
+  ]);
+
+  const [columnDefs] = useState([
+    { field: "ID",rowDrag:true },
+    { field: "Name" },
+    { field: "Extra_Hours" },
+    { field: "Manual_Hours" },
+    { field: "Monthly_Hours" },
+    { field: "Overall_Hours" }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="ag-theme-alpine" style={{ height: 400, width:"50%"  }}>
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={columnDefs}
+          rowDragManaged={true}
+          >
+        </AgGridReact>
+      </div>
+  )
 }
 
 export default App;
+
